ebd15681bb2082be06767f3ab900d4bcf389418d
cgruda <cgruda@gmail.com>
2021-05-26 00:12:03 +0300

    random port works. left only server init

--- main.c
+++ main.c
@@ -8,82 +8,104 @@
 #include <netdb.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include <stdint.h>
 
+#include <string.h>
+
+#include <time.h>
+
+#define ERROR			-1
+#define dbg			printf
 #define SERVER_PORT_PATH	"server_port"
 #define CLIENT_PORT_PATH	"http_port"
 #define TEMP_CLIENT_PORT	5025
-#define TEMP_SERVER_PORT	5026
+
+#ifdef RAND_MAX
+#undef RAND_MAX
+#endif
+
+#define RAND_MAX	64000 - RAND_MIN
+#define RAND_MIN	1024
 
 #define MAX_SERVERS			3
-#define SERVER_SOCKET_MAX_BACKLOG	3
+#define SOCKET_BACKLOG			3
 #define CLIENT_SOCKET_MAX_BACKLOG	1
 
 int write_port_to_file(uint16_t port, char *path)
 {
 	FILE *fp = fopen(path, "w");
 	if (!fp) {
-		strerror(errno);
-		return errno;
+		return ERROR;
 	}
 	fprintf(fp, "%d", port);
 	fclose(fp);
 	return 0;
 }
 
+int load_balancer_port_init(char *path, int *sockfd)
+{
+	ushort rand_port;
+
+	*sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (*sockfd < 0) {
+		return ERROR;
+	}
+
+	do {
+		rand_port = rand() + RAND_MIN;
+
+		struct sockaddr_in addr = {0};
+		addr.sin_family = AF_INET;
+		addr.sin_addr.s_addr = htonl(INADDR_ANY);
+		addr.sin_port = htons(rand_port);
+
+		if (!bind(*sockfd, (struct sockaddr *)&addr, sizeof(addr))) {
+			break;
+		}
+
+	} while (1);
+
+	if (write_port_to_file(rand_port, path) < 0) {
+		return ERROR;
+	}
+
+	if (listen(*sockfd, SOCKET_BACKLOG) < 0) {
+		return ERROR;
+	}
+
+	return 0;
+}
+
 int main()
 {
-	int status;
+	srand(time(0));
 
-	int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_sockfd < 0) {
-		strerror(errno);
-		return errno;
-	}
+	// int status;
+	int server_sockfd;
 
-	int client_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (client_sockfd < 0) {
-		strerror(errno);
-		return errno;
-	}
+	load_balancer_port_init(SERVER_PORT_PATH, &server_sockfd);
 
-	struct sockaddr_in server_addr = {0};
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(TEMP_SERVER_PORT);
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// int client_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	// if (client_sockfd < 0) {
+	// 	strerror(errno);
+	// 	return errno;
+	// }
 
-	struct sockaddr_in client_addr;
-	client_addr.sin_family = AF_INET;
-	client_addr.sin_port = htons(TEMP_CLIENT_PORT);
-	client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// struct sockaddr_in client_addr;
+	// client_addr.sin_family = AF_INET;
+	// client_addr.sin_port = htons(TEMP_CLIENT_PORT);
+	// client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	status = bind(server_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-	if (status < 0) {
-		strerror(errno);
-		return errno;
-	}
+	// status = bind(client_sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr));
+	// if (status < 0) {
+	// 	strerror(errno);
+	// 	return errno;
+	// }
 
-	status = bind(client_sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr));
-	if (status < 0) {
-		strerror(errno);
-		return errno;
-	}
-
-	status = listen(server_sockfd, SERVER_SOCKET_MAX_BACKLOG);
-	if (status < 0) {
-		strerror(errno);
-		return errno;
-	}
-
-	status = listen(client_sockfd, CLIENT_SOCKET_MAX_BACKLOG);
-	if (status < 0) {
-		strerror(errno);
-		return errno;
-	}
+	// status = listen(client_sockfd, CLIENT_SOCKET_MAX_BACKLOG);
+	// if (status < 0) {
+	// 	strerror(errno);
+	// 	return errno;
+	// }
 
 	int connfd[MAX_SERVERS] = {0};
 	socklen_t addrlen[MAX_SERVERS] = {0};
